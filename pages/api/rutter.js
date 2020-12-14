6f84da94237caaef1140648a7774ffd5d5fdfb65
cyrieu <cyrieu@gmail.com>
2020-12-14 11:17:36 -0800

    Working quickstart

--- pages/api/rutter.js
+++ pages/api/rutter.js
@@ -1,12 +1,42 @@
 import axios from 'axios';
 
+const ENV_URL = "sandbox.rutterapi.com"
+const CLIENT_ID = "YOUR_RUTTER_CLIENT_ID"
+const SECRET = "YOUR_RUTTER_SECRET_DO_NOT_PUBLISH";
+
 // handles exchanging the token and calling a sample API route
-export default (req, res) => {
+export default async (req, res) => {
   if (req.method === 'POST') {
     // Process a POST request
+    const { publicToken } = req.body;
+    // Exchange publictoken for access_token
+    try {
+      const response = await axios.post(`https://${ENV_URL}/item/public_token/exchange`, {
+        client_id: CLIENT_ID,
+        public_token: publicToken,
+        secret: SECRET,
+      })
+      const { data: { access_token }} = response;
+      const dataResponse = await axios.post(`https://${ENV_URL}/orders/get`, {
+        client_id: CLIENT_ID,
+        access_token,
+        secret: SECRET
+      })
+      const { data } = dataResponse;
+      res.statusCode = 200;
+      res.json({
+        ...data
+      })
+    } catch (e) {
+      console.error(e)
+      res.status(500).json({
+        error_message: "Rutter Error"
+      })
+    }
   } else {
     // Handle any other HTTP method
+    res.statusCode(401).json({
+      error_message: "Unauthorized Method"
+    })
   }
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,49 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useRutterLink } from 'react-rutter-link'
+import axios from 'axios';
+import React from 'react';
 
 export default function Home() {
+  const [dataFetched, setDataFetched] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
   const config = {
     publicKey: "YOUR_RUTTER_PUBLIC_KEY",
-    onSuccess: () => {
-      console.log("SUCCESS")
+    onSuccess: (publicToken) => {
+      // We call our NextJS backend API in pages/api/rutter.js
+      // It exchanges the publicToken for an access_token and makes an API call to /orders/get
+      setLoading(true);
+      axios.post('/api/rutter', {
+        publicToken
+      }).then(response => {
+        setDataFetched(response.data)
+      }).catch(e => {
+        console.error(e);
+      }).finally(() => {
+        setLoading(false)
+      })
     }
   }
   const { open, ready, error } = useRutterLink(config);
+
+  if (loading) {
+    return (
+      <div><div>Loading...</div></div>
+    )
+  }
+
+  if (dataFetched) {
+    return (
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          Response from /orders/get
+        </h1>
+        <textarea style={{marginTop: 20}} name="" id="" cols="80" rows="40" value={JSON.stringify(dataFetched, null, 4)}></textarea>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -24,7 +58,7 @@ export default function Home() {
 
         <div style={{ display: "flex", alignItems: "center"}} className={styles.description}>
           <div>
-            Test out Rutter Link & our endpoints:
+            Test out Rutter Link & API:
             {" "}
           </div>
           <div style={{ marginLeft: 8}}>
